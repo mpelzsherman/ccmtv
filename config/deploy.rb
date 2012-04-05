02c9601c7538002215fa5a220be47ae10f7719f3
Todd Blackman <todd.blackman@legitscript.com>
2012-04-04 22:00:44 -0700

    Deployment almost there.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,10 +6,18 @@ set :user, 'pianodbc'
 set :use_sudo, false
 set :deploy_to,  '/home3/pianodbc/pianodbv2'
 set :scm, :git
+set :bundle_cmd, 'source $HOME/.bashrc && /home3/pianodbc/ruby/gems/bin/bundle'
 
 role :web, "pianodb.com"                   # Your HTTP server, Apache/etc
 role :app, "pianodb.com"                   # This may be the same as your `Web` server
 role :db,  "pianodb.com", :primary => true # This is where Rails migrations will run
+
+after "deploy:update_code", :configure_database
+desc "copy databse.yml into the current release path"
+task :configure_database do
+  db_config = "#{deploy_to}/shared/database.yml"
+  run "cp #{db_config} #{release_path}/config/database.yml"
+end
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
