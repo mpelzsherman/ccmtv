2936935c5ca605efdb94e0fdbd5e829e09bb1823
Todd Blackman <todd.blackman@legitscript.com>
2012-05-20 21:34:45 -0700

    Some progress on people form and validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,11 +7,23 @@ class Person < ActiveRecord::Base
   scope :pianists,  where(:performer => true)
   scope :top10,     limit(10)
 
+  validates_uniqueness_of :canonical_name
+  validates_presence_of :canonical_name
+  validates_length_of :canonical_name, :within => 2..200
+  validate :categorised?
+
   def rank
     'N/A'
   end
 
   def views
     'N/A'
+  end
+
+  private
+
+  # Must be at least one of these.
+  def categorised?
+    errors[:base] << "You must make the person either a composer, performer, or both" unless composer || performer
   end
 end
