b574cbc4ce5ce9ac63a1d288a14ef9b8d6667bed
Michael Pelz-Sherman <mpelzsherman@primedia.com>
2012-11-15 13:57:44 -0500

    limit decoration to prevent redundant SQL queries

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,8 +1,8 @@
 class SiteController < ApplicationController
   def top10
-    @performances = PerformanceDecorator.decorate(Performance.top10)
+    @performances = PerformanceDecorator.decorate(Performance.top10.includes([:performer, :composer]))
     @composers    = Person.composers.top10
     @pianists     = Person.pianists.top10
-    @compositions = CompositionDecorator.decorate(Composition.top10)
+    @compositions = CompositionDecorator.decorate(Composition.top10.includes(:composer))
   end
 end
