64b73900ee241a5bd66d241ab9e76f911f7aefb3
Michael Pelz-Sherman <mpelzsherman@primedia.com>
2012-11-27 12:46:34 -0500

    Progress on new/edit composition form. Added symlinks for admin index and show routes to share code with non-admin views.

--- app/models/composition.rb
+++ app/models/composition.rb
@@ -1,6 +1,7 @@
 class Composition < ActiveRecord::Base
+  attr_accessible :person_id, :musical_key_id, :year_composed_begin, :year_composed_end, :rating, :catalog_num1, :catalog_num2, :catalog_num3, :views, :movement_num, :comp_group, :title, :movement_name, :rank
   belongs_to :composer, :class_name => 'Person', :foreign_key => :person_id
-  belongs_to :musical_key
+  belongs_to :musical_key, :class_name => 'MusicalKey', :foreign_key => :musical_key_id
 
   scope :top10, limit(10)
 end
