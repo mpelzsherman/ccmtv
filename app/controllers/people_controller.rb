defde0992916399da56992df6ab1b2f0928f3921
mpelzsherman <mpelzsherman@gmail.com>
2013-01-02 03:00:27 -0800

    more non-admin routes/views for viewing composers and performers

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,14 +1,15 @@
 class PeopleController < ApplicationController
   def index
     params[:person] ||= {}
-    params[:person][:composer]  ||= '1'
-    params[:person][:performer] ||= '1'
+    #params[:person][:composer]  ||= '1'
+    #params[:person][:performer] ||= '1'
 
     @people = Person.search(params[:person]).by_canonical_name.paginate(:page => params[:page])
     @person = Person.new(params[:person])
   end
 
   def show
+    @person = Person.find(params[:id])
   end
 
   def composers
