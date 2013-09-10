bddf031dc1ac34734bc9bf90e46d1f68a7b7e9b9
mpelzsherman <mpelzsherman@gmail.com>
2013-09-10 08:33:54 -0400

    remove has_and_belongs_to_many :urls relation from performance model

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -5,7 +5,6 @@ class Performance < ActiveRecord::Base
   belongs_to :performance_type
   belongs_to :performer, :class_name => 'Person', :foreign_key => :person_id
   belongs_to :url
-  has_and_belongs_to_many :urls
   scope :top10, limit(10)
   accepts_nested_attributes_for :composer, :performer, :composition, :url
   has_many :performance_roles, :dependent => :destroy
